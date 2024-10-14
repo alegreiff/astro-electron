189cd873d8c513a5aa8c1ca300122160f5d80067
Jaime de Greiff <alegreiff@gmail.com>
2024-10-14 18:33:43 -0500

    Inicio electron + astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  srcDir: './src/astro-ui',
+  outDir: './dist-astro',
+  publicDir: './src/public',
+
+  server: {
+    port: 4321,
+    
+  },
+
+  integrations: [tailwind()]
+});
---
+++ src/electron/main.ts
@@ -0,0 +1,14 @@
+import {app, BrowserWindow  } from 'electron';
+import path from 'path';
+import { isDev } from './util.js';
+
+
+
+app.on("ready", () => {
+  const mainWindow = new BrowserWindow({})
+  if(isDev()){ 
+    mainWindow.loadURL("http://localhost:4321");
+  } else {
+    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-astro/index.html"))
+  }
+})
